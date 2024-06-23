213c36ccc62e37e36f6cfce9dedc6033fd72bafe
Kenth Saya-ang <gargarkenth93@gmail.com>
2024-06-23 15:46:59 +0000

    init comit

---
+++ index.js
@@ -0,0 +1,52 @@
+// index.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// your first API endpoint... 
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: 'hello API' });
+});
+
+
+const isInvalidDate = (date) => {
+  date.toUTCString() === "Invalid Date"
+}
+
+app.get("/api/:date", function (req, res) {
+  const dateParam = req.params.date;
+  console.log(req.params.date);
+
+
+  res.json({ date: dateParam });
+});
+
+app.get("/api", (req, res) => {
+  res.json({
+    unix: new Date().toUTCString(),
+    utc: new Date().getTime()
+  })
+})
+
+
+
+// Listen on port set in environment variable or default to 3000
+var listener = app.listen(process.env.PORT || 3000, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
